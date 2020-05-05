13ea15ba0c18269197dc69607e0aa4624f64759d
PavelAndreyuk <58653747+PavelAndreyuk@users.noreply.github.com>
2020-05-05 18:36:14 +0400

    Add files via upload

---
+++ train_v2.py
@@ -0,0 +1,33 @@
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from model_v2 import load_model
+import numpy as np
+
+X_train=np.load('train.npy')
+frames=X_train.shape[2]
+
+frames=frames-frames%10
+
+X_train=X_train[:,:,:frames]
+X_train=X_train.reshape(-1,227,227,10)
+X_train=np.expand_dims(X_train,axis=4)
+Y_train=X_train.copy()
+
+epochs=2
+batch_size=1
+
+if __name__=="__main__":
+
+	model=load_model()
+	print(model.summary())
+
+	callback_save = ModelCheckpoint("123.h5", monitor='loss', save_best_only=True)
+
+	callback_early_stopping = EarlyStopping(monitor='loss', patience=3)
+
+	print('Model has been loaded')
+
+	model.fit(X_train,Y_train,
+			  batch_size=batch_size,
+			  epochs=epochs,
+			  callbacks = [callback_save,callback_early_stopping]
+			  )
